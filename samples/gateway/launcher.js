48528a27ab1e4e26e0235eaeba19e8dfadc9202f
etissieres <etienne.tissieres@gmail.com>
2013-11-20 18:01:48 +0100

    gateway fixes & evolution

--- samples/gateway/launcher.js
+++ samples/gateway/launcher.js
@@ -6,11 +6,16 @@ var hubiquitus = require("hubiquitus-core");
 var gateway = require(__dirname + "/../../lib/gateway");
 
 var logger = hubiquitus.logger;
-logger.level = "info";
+logger.level = "trace";
+
+hubiquitus.start()
+  .addActor("ping", function (from, content, reply) {
+    logger.info(this.id + "> from " + from + " : " + content);
+    reply(null, content);
+  });
 
 // gateway creation; does'nt provide implementation : will use default in-memory one
 var gat = gateway.createGateway();
-gat.start();
 
 gat.on("started", function () {
   logger.info("gateway started");
@@ -19,3 +24,13 @@ gat.on("started", function () {
 gat.on("error", function (err) {
   logger.err(err);
 });
+
+gat.on("connected", function (aid) {
+  logger.info(aid + " connected !");
+});
+
+gat.on("disconnected", function (aid) {
+  logger.info(aid + " disconnected !");
+});
+
+gat.start();
